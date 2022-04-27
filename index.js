1734a5da9b94107184aef397cf6b2c009fdce503
George Escobar <geo.escobar214@gmail.com>
2022-04-27 17:48:23 -0500

    index.js file almost completed

--- index.js
+++ index.js
@@ -1,34 +1,153 @@
-// variables 
-const inquirer = require('inquirer');
-const Manager = require('./team/manager.js');
-const Engineer = require('./team/engineer.js');
-const Intern = require('./team/intern.js');
+// variables
+const inquirer = require("inquirer");
+const Manager = require("./team/manager.js");
+const Engineer = require("./team/engineer.js");
+const Intern = require("./team/intern.js");
 const team = [];
-const generate = require('./output/generate.js'); 
-const fs = require('fs');
+const generate = require("./output/generate.js");
+const fs = require("fs");
 
-
-// manager prompts 
+// manager prompts
 const promptMgr = () => {
-    rerutrn.inquirer.prompt ([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-        }, {
-            type: 'input',
-            name: 'mgrId',
-            message: 'What is your employee ID?'
-        }, {
-            type: 'input',
-            name: 'mgrEmail',
-            message: 'What is your email?'
-        }, {
-            type: 'input',
-            name: 'officeNum',
-            message: 'What is your office number?'
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+      },
+      {
+        type: "input",
+        name: "mgrId",
+        message: "What is your employee ID?",
+      },
+      {
+        type: "input",
+        name: "mgrEmail",
+        message: "What is your email?",
+      },
+      {
+        type: "input",
+        name: "officeNum",
+        message: "What is your office number?",
+      },
     ])
-   
-    
-}
+    .then((answers) => {
+      console.log(answers);
+      //   const mgr = new Manager(answers.name, answers.mgrId, answers.mgrEmail, answers.officeNum);
+      //   team.push(mgr);
+      promptMenu();
+    });
+};
+
+// menu prompt
+const promptMenu = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "menu",
+        name: "menu",
+        message: "Who would you like to add to your team?",
+        choices: ["Add engineer", "Add intern", "Finished building my team"],
+      },
+    ])
+    .then((userChoice) => {
+      if (userChoice === "Add engineer") {
+        promptEngineer();
+      }
+      if (userChoice === "Add intern") {
+        promptIntern();
+      }
+      if (userChoice === "Finished building my team") {
+        generateTeam();
+      }
+    });
+};
+
+// engineer prompt
+const promptEngineer = () => {
+  console.log(`
+    ****************
+    Add new engineer 
+    ****************
+    `);
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the engineers name?",
+      },
+      {
+        type: "input",
+        name: "employeeId",
+        message: "What is their employee ID?",
+      },
+      {
+        type: "input",
+        name: "employeeEmail",
+        message: "What is their employee email?",
+      },
+      {
+        type: "input",
+        name: "employeeGit",
+        message: "What is the link to their Github?",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      //   const engineer = new Engineer(answers.name, answers.employeeId, answers.employeeEmail, answers.employeeGit);
+      //   team.push(engineer);
+      promptMenu();
+    });
+};
+
+// intern prompt
+const promptIntern = () => {
+  console.log(`
+    ****************
+    Add new intern 
+    ****************
+    `);
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the interns name?",
+      },
+      {
+        type: "input",
+        name: "employeeId",
+        message: "What is their employee ID?",
+      },
+      {
+        type: "input",
+        name: "employeeEmail",
+        message: "What is their employee email?",
+      },
+      {
+        type: "input",
+        name: "employeeGit",
+        message: "What is the link to their Github?",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      //   const intern = new Intern(answers.name, answers.employeeId, answers.employeeEmail, answers.employeeGit);
+      //   team.push(intern);
+      promptMenu();
+    });
+};
+
+const generateTeam = () => {
+  console.log(`
+    ******
+    Completed building my team
+    ******
+    `);
+
+};
+
+// invoke function
+promptMgr();
